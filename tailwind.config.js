4c29353c8f82c008f3860f1f6bd40b5ebd96d407
Likhon-Official <LikhonOfficial@hotmail.com>
2024-11-03 20:39:56 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,36 +10,11 @@ export default {
         'mono': ['Space Mono', 'monospace'],
         'sans': ['Space Mono', 'monospace'],
       },
-      fontSize: {
-        'xs': '0.75rem',
-        'sm': '0.875rem',
-        'base': '1rem',
-        'lg': '1.125rem',
-        'xl': '1.25rem',
-        '2xl': '1.5rem',
-        '3xl': '1.875rem',
-        '4xl': '2.25rem',
-        '5xl': '3rem',
-        '6xl': '3.75rem',
+      aspectRatio: {
+        'square': '1 / 1',
       },
-      spacing: {
-        '18': '4.5rem',
-        '22': '5.5rem',
-        '30': '7.5rem',
-      },
-      maxWidth: {
-        'screen-xl': '1280px',
-        'screen-2xl': '1536px',
-      },
-      container: {
-        center: true,
-        padding: {
-          DEFAULT: '1rem',
-          sm: '2rem',
-          lg: '4rem',
-          xl: '5rem',
-          '2xl': '6rem',
-        },
+      borderRadius: {
+        '2xl': '1rem',
       },
     },
   },
